71c5d9534eae37a258a7a3e6d56f5da8d922e6c6
lix <xuzenglin@gmail.com>
2020-04-17 10:49:16 +0800

    add named parameters for connect to server, such as ssl

---
+++ README.md
@@ -0,0 +1,2 @@
+# ldap3tool
+Simple wrapper for ldap3
--- setup.py
+++ setup.py
@@ -3,31 +3,41 @@ ldap3tool
 --------------
 Simple wrapper for ldap3
 """
+import io
+import os.path
 from setuptools import setup
 
+work_dir = os.path.dirname(os.path.abspath(__file__))
+fp = os.path.join(work_dir, "ldap3tool/__init__.py")
+
+with io.open(fp, encoding="utf-8") as f:
+    for line in f:
+        if line.startswith("__version__ = "):
+            version = line.split("=")[-1].strip().replace("'", "")
+            break
+
 setup(
-    name='ldap3tool',
-    version='0.4.1',
-    url='https://github.com/lixxu/ldap3tool',
-    license='BSD',
-    author='Lix Xu',
-    author_email='xuzenglin@gmail.com',
-    description='Simple wrapper for ldap3',
+    name="ldap3tool",
+    version=version.replace('"', ""),
+    url="https://github.com/lixxu/ldap3tool",
+    license="BSD",
+    author="Lix Xu",
+    author_email="xuzenglin@gmail.com",
+    description="Simple wrapper for ldap3",
     long_description=__doc__,
-    packages=['ldap3tool'],
+    packages=["ldap3tool"],
     zip_safe=False,
-    platforms='any',
-    install_requires=['ldap3'],
+    platforms="any",
+    install_requires=["ldap3"],
     classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ]
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
 )
